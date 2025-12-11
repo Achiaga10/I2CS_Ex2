f5498f50e2f2cc57c528b26531cdb0438eeb18db
Achiaga10 <achia.gabbay@gmail.com>
2025-12-11 13:30:09 +0200

    completed all functions.

--- src/assignments/Ex2/Index2D.java
+++ src/assignments/Ex2/Index2D.java
@@ -1,41 +1,57 @@
 package assignments.Ex2;
 
 public class Index2D implements Pixel2D {
+    private int _x;
+    private int _y;
+
     public Index2D(int w, int h) {
-        ;
+        _x=w;
+        _y=h;
     }
     public Index2D(Pixel2D other) {
-        ;
+        this._x = other.getX();
+        this._y = other.getY();
     }
     @Override
     public int getX() {
-
-        return 0;
+        return this._x;
     }
 
     @Override
     public int getY() {
-
-        return 0;
+        return this._y;
     }
 
     @Override
     public double distance2D(Pixel2D p2) {
+        if (p2 == null) {
+            throw new RuntimeException("Cannot calculate distance to a null Pixel2D object.");
+        }
 
-        return 0;
+        double dx = this._x - p2.getX();
+        double dy = this._y - p2.getY();
+
+        return Math.hypot(dx, dy);
     }
 
     @Override
     public String toString() {
-        String ans = null;
-
+        String ans = "x: " + this._x + ", y: " + this._y;
         return ans;
     }
 
     @Override
     public boolean equals(Object p) {
-        boolean ans = true;
-
-        return ans;
+        if (this == p) {
+            return true;
+        }
+        if (!(p instanceof Pixel2D)) {
+            return false;
+        }
+        Pixel2D p1 = (Pixel2D) p;
+        if  (this._x == p1.getX() && this._y == p1.getY()) {
+            return true;
+        }
+        return false;
     }
 }
